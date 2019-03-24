1437121fe99c4ac8a316e193d1a843e9ca7a1e7e
Peerlator <peer.rheinboldt@gmail.com>
2019-03-24 21:00:10 +0100

    Added source to script calling

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ sudo chmod +x $HOME/.dotfiles/*.sh
 sudo chmod +x $HOME/.dotfiles/scripts/*
 # Install files
 echo "Installing general things (can be found in general_installs.sh)"
-$HOME/.dotfiles/general_installs.sh
-$HOME/.dotfiles/app_installs.sh
+source $HOME/.dotfiles/general_installs.sh
+source $HOME/.dotfiles/app_installs.sh
 export PATH=$PATH:$DOTFILES/scripts
 
 echo "Installing specific things (can be found in the install.sh files)"
 for filename in $(echo $HOME/.dotfiles/**/install.sh | tr " " "\n")
 do
-    sh $filename
+    source $filename
 done
